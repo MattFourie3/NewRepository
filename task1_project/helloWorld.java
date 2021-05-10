a0c759f5746690ae00e0eeaf7c69c5b313493b64
Matthew <mattfourie3@gmail.com>
2021-05-10 16:58:07 +0200

    Edited script to ask user input

--- task1_project/helloWorld.java
+++ task1_project/helloWorld.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class helloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Git is Awesome!");
+	public static void main(String[] args)  
+	{  
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);    //System.in is a standard input stream  
+		System.out.print("What is your name?: ");  
+		String a = sc.nextLine();
+		
+		System.out.println(a); 
 
 	}
 
